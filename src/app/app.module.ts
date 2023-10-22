5f4085b29aab8eff034705c7d2577f0f584d732a
maxfideles <maxfideles24@gmail.com>
2023-10-22 20:43:48 -0300

    Menu with Styles and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PanelsComponent } from './pages/panels/panels.component';
 import { InvertersComponent } from './pages/inverters/inverters.component';
 import { MenuComponent } from './menu/menu.component';
+import { MenuOptionsComponent } from './menu/menu-options/menu-options.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +16,14 @@ import { MenuComponent } from './menu/menu.component';
     HomeComponent,
     PanelsComponent,
     InvertersComponent,
-    MenuComponent
+    MenuComponent,
+    MenuOptionsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot([
-      {path:'', component: HomeComponent},
+      {path:'home', component: HomeComponent},
       {path:'panels', component: PanelsComponent},
       {path: 'inverters', component: InvertersComponent}
 
