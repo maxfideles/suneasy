5392aaf13fe8a59cd29f7913438d94461c1058bb
maxfideles <maxfideles24@gmail.com>
2023-10-22 19:38:50 -0300

    RouterModule with Paths Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule } from '@angular/common/http'
-
+import { RouterModule} from '@angular/router'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
@@ -18,6 +18,12 @@ import { InvertersComponent } from './pages/inverters/inverters.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'', component: HomeComponent},
+      {path:'panels', component: PanelsComponent},
+      {path: 'inverters', component: InvertersComponent}
+
+    ]),
     HttpClientModule
   ],
   providers: [],
