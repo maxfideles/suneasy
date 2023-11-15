18783357efa6eb405fa8e31e42e14f3f571b2305
maxfideles <maxfideles24@gmail.com>
2023-11-15 14:06:08 -0300

    City Model

---
+++ src/app/models/cityData.ts
@@ -0,0 +1,58 @@
+export interface City {
+     city : {
+         id : number,
+         name :  string ,
+         state :  string ,
+         lat : number,
+         lon : number,
+         temp : number
+    },
+     diffuseList : {
+         id : number,
+         annual : number,
+         feb : number,
+         jan : number,
+         mar : number,
+         apr : number,
+         may : number,
+         jun : number,
+         jul : number,
+         aug : number
+         sep : number,
+         oct : number,
+         nov : number,
+         dec : number
+    },
+     horizontalList : {
+         id : number,
+         annual : number,
+         jan : number,
+         feb : number,
+         mar : number,
+         apr : number,
+         may : number,
+         jun : number,
+         jul : number,
+         aug : number,
+         sep : number,
+         oct : number,
+         nov : number,
+         dec : number
+    },
+     tiltedLatList : {
+         id : number,
+         annual : number,
+         jan : number,
+         feb : number,
+         mar : number,
+         apr : number,
+         may : number,
+         jun : number,
+         jul : number,
+         aug : number,
+         sep : number,
+         oct : number,
+         nov : number,
+         dec : number
+    }
+}
