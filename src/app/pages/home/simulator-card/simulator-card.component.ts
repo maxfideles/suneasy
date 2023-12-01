2653b4fc2b4ba8f235b89a0c2c3f7c8e46579a3c
maxfideles <maxfideles24@gmail.com>
2023-11-30 21:02:31 -0300

    Added

---
+++ src/app/pages/home/simulator-card/simulator-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-simulator-card',
+  templateUrl: './simulator-card.component.html',
+  styleUrls: ['./simulator-card.component.css']
+})
+export class SimulatorCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
